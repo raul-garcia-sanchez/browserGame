f21f3aa101c3ba88071c602b38d3bd050779b4e2
R0sac <oscarhortelano2001@gmail.com>
2023-04-27 17:50:13 +0200

    end

--- WizardBrowserGame/game/admin.py
+++ WizardBrowserGame/game/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Log)
+admin.site.register(Opcions_Global)
+admin.site.register(Accion)
+admin.site.register(Event)
+
+
--- WizardBrowserGame/game/models.py
+++ WizardBrowserGame/game/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Log(models.Model):
+
+    def __str__(self):
+        return self
+
+class Opcions_Global(models.Model):
+    
+    def __str__(self):
+        return self
+    
+class Accion(models.Model):
+    
+    def __str__(self):
+        return self
+
+class Event(models.Model):
+    
+    def __str__(self):
+        return self
