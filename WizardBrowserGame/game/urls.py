e018df829fb3fed14d27e7a3c00323943d686ff8
R0sac <oscarhortelano2001@gmail.com>
2023-04-27 17:26:43 +0200

    areas del frondend

--- WizardBrowserGame/game/urls.py
+++ WizardBrowserGame/game/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cron/', views.cron, name='cron'),
+    path('play_action/', views.play_action, name='play_action'),
+    path('messages/', views.messages, name='messages'),
+    path('ranking/', views.ranking, name='ranking'),
 ]
--- WizardBrowserGame/game/views.py
+++ WizardBrowserGame/game/views.py
@@ -5,3 +5,23 @@ def index(request):
     context = {}
 
     return render(request, 'index/index.html', context)
+
+def cron(request):
+    context = {}
+
+    return render(request, 'index/cron.html', context)
+
+def play_action(request):
+    context = {}
+
+    return render(request, 'index/play_action.html', context)
+
+def messages(request):
+    context = {}
+
+    return render(request, 'index/messages.html', context)
+
+def ranking(request):
+    context = {}
+
+    return render(request, 'index/ranking.html', context)
