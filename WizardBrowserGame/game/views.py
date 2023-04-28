a90fb49c74efe39a9b317035f583c5b9b0d0d7d7
raul-garcia-sanchez <raulgarcia200326@gmail.com>
2023-04-28 22:22:27 +0200

    cambiar mensajes de error

--- WizardBrowserGame/game/views.py
+++ WizardBrowserGame/game/views.py
@@ -29,10 +29,8 @@ def changePassword(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)  # Important!
-            messages.success(request, 'Your password was successfully updated!')
-            #return redirect('change_password')
-        else:
-            messages.error(request, 'Please correct the error below.')
+            return redirect('change_password')
+        #canviar mensajes de error
     else:
         form = PasswordChangeForm(request.user)
     return render(request, "registration/changePassword.html", {
