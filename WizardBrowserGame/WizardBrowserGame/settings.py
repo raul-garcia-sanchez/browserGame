f4ea0fa60b23a3298845ac463d40b58af954fecd
Raúl García Sánchez <raulgarcia200326@gmail.com>
2023-05-08 19:59:02 +0200

    to railway in progress

--- WizardBrowserGame/WizardBrowserGame/settings.py
+++ WizardBrowserGame/WizardBrowserGame/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'public/static/')
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/media/')
--- WizardBrowserGame/WizardBrowserGame/urls.py
+++ WizardBrowserGame/WizardBrowserGame/urls.py
@@ -49,6 +49,3 @@ urlpatterns = [
 
     path("accounts/", include("django.contrib.auth.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
